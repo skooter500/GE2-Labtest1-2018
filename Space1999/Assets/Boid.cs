85b56f74202bdd951612dabc5377c360fa7bd556
Bryan Duggan <skooter500@gmail.com>
2018-03-13 15:49:02 +0000

    Added solution

---
+++ Space1999/Assets/Boid.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    List<SteeringBehaviour> behaviours = new List<SteeringBehaviour>();
+    
+    public Vector3 force = Vector3.zero;
+    public Vector3 acceleration = Vector3.zero;
+    public Vector3 velocity = Vector3.zero;
+    public float mass = 1;
+    public float maxSpeed = 5.0f;   
+    
+    // Use this for initialization
+    void Start () {
+
+        SteeringBehaviour[] behaviours = GetComponents<SteeringBehaviour>();
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            this.behaviours.Add(b);
+        }
+	}
+
+    public Vector3 SeekForce(Vector3 target)
+    {
+        Vector3 desired = target - transform.position;
+        desired.Normalize();
+        desired *= maxSpeed;
+        return desired - velocity;
+    }
+
+    public Vector3 ArriveForce(Vector3 target, float slowingDistance = 15.0f, float deceleration = 1.0f)
+    {
+        Vector3 toTarget = target - transform.position;
+
+        float distance = toTarget.magnitude;
+        if (distance == 0)
+        {
+            return Vector3.zero;
+        }
+        float ramped = maxSpeed * (distance / (slowingDistance * deceleration));
+
+        float clamped = Mathf.Min(ramped, maxSpeed);
+        Vector3 desired = clamped * (toTarget / distance);
+
+        return desired - velocity;
+    }
+
+
+    Vector3 Calculate()
+    {
+        force = Vector3.zero;
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            if (b.isActiveAndEnabled)
+            {
+                force += b.Calculate() * b.weight;
+            }
+        }
+
+        return force;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        force = Calculate();
+        acceleration = force / mass;
+
+        velocity += acceleration * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        Vector3 globalUp = new Vector3(0, 0.2f, 0);
+        Vector3 accelUp = acceleration * 0.05f;
+        Vector3 bankUp = accelUp + globalUp;
+        float smoothRate = Time.deltaTime * 3f;
+        Vector3 tempUp = transform.up;
+        tempUp = Vector3.Lerp(tempUp, bankUp, smoothRate);
+        if (velocity.magnitude  > 0.0001f)
+        {
+            transform.LookAt(transform.position + velocity);
+            transform.position += velocity * Time.deltaTime;
+        }        
+	}
+}
---
+++ Space1999/Assets/EagleSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+    public float gap = 20;
+    public float followers = 2;
+    public GameObject prefab;
+
+    private void Start()
+    {
+        GameObject leader = GameObject.Instantiate<GameObject>(prefab);
+        leader.transform.parent = this.transform;
+        leader.transform.position = this.transform.position;
+        leader.transform.rotation = this.transform.rotation;
+
+        Seek seek = leader.AddComponent<Seek>();
+        seek.target = leader.transform.position + leader.transform.forward * 1000;
+
+        FollowCamera fc = FindObjectOfType<FollowCamera>();
+        fc.target = leader;
+
+        for (int i = 1; i <=followers; i++)
+        {
+            Vector3 offset = new Vector3(gap * i, 0, - gap * i);
+            GameObject follower = CreateFollower(offset, leader.GetComponent<Boid>());
+            offset = new Vector3(- gap * i, 0, - gap * i);
+            follower = CreateFollower(offset, leader.GetComponent<Boid>());            
+        }
+    }
+
+    GameObject CreateFollower(Vector3 offset, Boid leader)
+    {
+        GameObject follower = GameObject.Instantiate<GameObject>(prefab);
+        follower.transform.position = this.transform.TransformPoint(offset);
+        follower.transform.parent = this.transform;
+        follower.transform.rotation = this.transform.rotation;
+        OffsetPursue op = follower.AddComponent<OffsetPursue>();
+        op.leader = leader;
+        return follower;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
